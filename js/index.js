0d2e6c7b1c238147ae8616aa8a1fefc7c6ab2923
MIKA ARAI <mika8500@yk.commufa.jp>
2023-03-25 15:31:36 +0900

    画像

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// パララックス
+
+//ES6 import
+import simpleParallax from 'simple-parallax-js';
+
+//CommonJS
+const simpleParallax = require('simple-parallax-js');
+
+const image = document.getElementsByClassName('kv__image--03');
+new simpleParallax(image,{
+    operation: 'right',
+    scale: 1.5,
+    delay: 2,
+    transition: 'cubic-bezier(0,0,0,.1)',
+    maxTransition: 40,
+    overflow: true
+});
