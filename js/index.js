b47ca6e83559b26e03d5064b72a63156abdf281f
MIKA ARAI <mika8500@yk.commufa.jp>
2023-03-26 14:50:46 +0900

    PCデザイン（）

--- js/index.js
+++ js/index.js
@@ -8,10 +8,9 @@ const simpleParallax = require('simple-parallax-js');
 
 const image = document.getElementsByClassName('kv__image--03');
 new simpleParallax(image,{
-    operation: 'right',
+    operation: 'up',
     scale: 1.5,
     delay: 2,
     transition: 'cubic-bezier(0,0,0,.1)',
-    maxTransition: 40,
-    overflow: true
+    maxTransition: 40
 });
